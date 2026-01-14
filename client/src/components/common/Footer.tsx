61d62c937b82e71151210c186989b9a5373464b6
Priyanshi Govil <priyanshigovil79@gmail.com>
2026-01-14 10:26:46 +0530

    Initial working version

---
+++ client/src/components/common/Footer.tsx
@@ -0,0 +1,17 @@
+function Footer() {
+    return (
+        <footer className="static bottom-1 left-0 flex w-full justify-center sm:fixed">
+            <span>
+                Made with ❤️ by{" "}
+                <a  target="_blank"
+                    href="https://github.com/PriyanshiGovil"
+                    className="text-st no-underline"
+                >
+                    Priyanshi Govil
+                </a>
+            </span>
+        </footer>
+    )
+}
+
+export default Footer
